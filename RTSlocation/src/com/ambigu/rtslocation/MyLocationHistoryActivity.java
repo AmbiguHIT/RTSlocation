e7b23b9a0e5d52aabf628b822658052b4b0f8839
29616 <29616@DESKTOP-RPNFI38>
2018-05-27 11:26:42 +0800

    2018.05.27

---
+++ RTSlocation/src/com/ambigu/rtslocation/MyLocationHistoryActivity.java
@@ -0,0 +1,45 @@
+package com.ambigu.rtslocation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyLocationHistoryActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	
+}
