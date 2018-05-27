63d57657bb5b15a043b58424b4451e819a81f952
29616 <29616@DESKTOP-RPNFI38>
2018-05-27 23:08:28 +0800

    first

--- RTSlocation/src/com/ambigu/adapter/AuthInfoAdapter.java
+++ RTSlocation/src/com/ambigu/adapter/AuthInfoAdapter.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import java.util.Map;
 
 import com.ambigu.client.RTSClient;
+import com.ambigu.listener.OnDelAuthInfoListener;
 import com.ambigu.model.AuthNode;
 import com.ambigu.model.Info;
 import com.ambigu.rtslocation.R;
@@ -33,105 +34,109 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 @SuppressLint("UseSparseArrays")
-public class AuthInfoAdapter extends BaseExpandableListAdapter implements HeaderAdapter{
-	
-	private HashMap<String,ArrayList<AuthNode>> childNodes;
+public class AuthInfoAdapter extends BaseExpandableListAdapter implements HeaderAdapter, OnDelAuthInfoListener {
+
+	private HashMap<String, ArrayList<AuthNode>> childNodes;
 	private ArrayList<String> groups;
 	private Context context;
-	//private ArrayList<HashMap<Integer, Boolean>> isVisable;
+	// private ArrayList<HashMap<Integer, Boolean>> isVisable;
 	private ArrayList<HashMap<Integer, Boolean>> isChecked;
-	private Button sure,cancel;
-	private boolean isMulChoice=false;
+	private Button sure, cancel;
+	private boolean isMulChoice = false;
+	private boolean isAllowOption = true;
 
-	public AuthInfoAdapter(HashMap<String,ArrayList<AuthNode>> childNodes,ArrayList<String> groups,Context context,Button sure,Button cancel) {
+	public AuthInfoAdapter(HashMap<String, ArrayList<AuthNode>> childNodes, ArrayList<String> groups, Context context,
+			Button sure, Button cancel) {
 		// TODO Auto-generated constructor stub
-		this.childNodes=childNodes;
-		this.context=context;
-		this.groups=groups;
-		this.sure=sure;
-		this.cancel=cancel;
-		//isVisable=new ArrayList<HashMap<Integer, Boolean>>();
-		this.isChecked=new ArrayList<HashMap<Integer, Boolean>>();
+		this.childNodes = childNodes;
+		this.context = context;
+		this.groups = groups;
+		this.sure = sure;
+		this.cancel = cancel;
+		// isVisable=new ArrayList<HashMap<Integer, Boolean>>();
+		this.isChecked = new ArrayList<HashMap<Integer, Boolean>>();
 		initView();
 	}
 
 	private void initView() {
 		// TODO Auto-generated method stub
 		sure.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				//确定删除弹框
+				// 确定删除弹框
 				AlertDialog.Builder builder = new Builder(context);
-				//设置对话框图标，可以使用自己的图片，Android本身也提供了一些图标供我们使用
+				// 设置对话框图标，可以使用自己的图片，Android本身也提供了一些图标供我们使用
 				builder.setIcon(R.drawable.ic_launcher);
-				//设置对话框标题
+				// 设置对话框标题
 				builder.setTitle("提示信息");
-				//设置对话框内的文本
+				// 设置对话框内的文本
 				builder.setMessage("确定删除这些记录吗？");
-				//设置确定按钮，并给按钮设置一个点击侦听，注意这个OnClickListener使用的是DialogInterface类里的一个内部接口
+				// 设置确定按钮，并给按钮设置一个点击侦听，注意这个OnClickListener使用的是DialogInterface类里的一个内部接口
 				builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-				        @Override
-				        public void onClick(DialogInterface dialog, int which) {
-				        	dialog.dismiss();
-				        	HashMap<String,ArrayList<AuthNode>> selects=new HashMap<String, ArrayList<AuthNode>>();
-				        	for(int i=0;i<groups.size();i++){//分组
-				        		ArrayList<AuthNode> authNodes=new ArrayList<AuthNode>();
-				        		String groupname = groups.get(i);
-				        		selects.put(groupname, authNodes);
-				        		HashMap<Integer, Boolean> maps=isChecked.get(i);
-				        		Iterator<Map.Entry<Integer, Boolean>> iterator=maps.entrySet().iterator();
-				        		while(iterator.hasNext()){
-				        			Map.Entry<Integer, Boolean> entry=iterator.next();
-				        			if(entry.getValue()){//此项被选中
-				        				int childpos=entry.getKey();
-				        				authNodes.add(childNodes.get(groupname).get(childpos));
-				        			}
-				        		}
-				        	}
-				        	Info info=new Info();
-				        	info.setFromUser(ApplicationVar.getId());
-				        	info.setInfoType(EnumInfoType.DEL_AUTH_NOTE);
-				        	info.setState(false);
-				        	info.setAuthNodes(selects);//删除这些
-				        	RTSClient.writeAndFlush(info);
-				        }
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.dismiss();
+						HashMap<String, ArrayList<AuthNode>> selects = new HashMap<String, ArrayList<AuthNode>>();
+						for (int i = 0; i < groups.size(); i++) {// 分组
+							ArrayList<AuthNode> authNodes = new ArrayList<AuthNode>();
+							String groupname = groups.get(i);
+							selects.put(groupname, authNodes);
+							HashMap<Integer, Boolean> maps = isChecked.get(i);
+							Iterator<Map.Entry<Integer, Boolean>> iterator = maps.entrySet().iterator();
+							while (iterator.hasNext()) {
+								Map.Entry<Integer, Boolean> entry = iterator.next();
+								if (entry.getValue()) {// 此项被选中
+									int childpos = entry.getKey();
+									authNodes.add(childNodes.get(groupname).get(childpos));
+								}
+							}
+						}
+						Info info = new Info();
+						info.setFromUser(ApplicationVar.getId());
+						info.setInfoType(EnumInfoType.DEL_AUTH_NOTE);
+						info.setState(false);
+						info.setAuthNodes(selects);// 删除这些
+						RTSClient.writeAndFlush(info);
+						isMulChoice = false;
+						isAllowOption = false;
+						notifyDataSetChanged();// 此时不应再进行操作
+					}
 				});
 				builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-				        @Override
-				        public void onClick(DialogInterface dialog, int which) {
-				        	dialog.dismiss();
-				        	
-				        }
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.dismiss();
+
+					}
 				});
-				
-				//使用builder创建出对话框对象
+
+				// 使用builder创建出对话框对象
 				AlertDialog dialog = builder.create();
-				//显示对话框
+				// 显示对话框
 				dialog.show();
 			}
 		});
-		
+
 		cancel.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-	        	isMulChoice=false;
-	        	for(int i=0;i<groups.size();i++){
-					String groupname=groups.get(i);
-					int size=childNodes.get(groupname).size();
+				isMulChoice = false;
+				for (int i = 0; i < groups.size(); i++) {
+					String groupname = groups.get(i);
+					int size = childNodes.get(groupname).size();
 					HashMap<Integer, Boolean> maps = isChecked.get(i);
-					for(int j=0;j<size;j++){
+					for (int j = 0; j < size; j++) {
 						maps.put(j, false);
 					}
 				}
 				notifyDataSetChanged();
 			}
 		});
-		
-		
+
 	}
 
 	@Override
@@ -179,73 +184,76 @@ public class AuthInfoAdapter extends BaseExpandableListAdapter implements Header
 	@Override
 	public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
-		if(convertView==null){
-			convertView=LayoutInflater.from(context).inflate(R.layout.auth_history_group, null);
+		if (convertView == null) {
+			convertView = LayoutInflater.from(context).inflate(R.layout.auth_history_group, null);
 		}
-		TextView groupnametv=(TextView) convertView.findViewById(R.id.groupname);
-		ImageView groupIcon=(ImageView) convertView.findViewById(R.id.groupIcon);
+		TextView groupnametv = (TextView) convertView.findViewById(R.id.groupname);
+		ImageView groupIcon = (ImageView) convertView.findViewById(R.id.groupIcon);
 		String groupname = groups.get(groupPosition);
 		groupnametv.setText(groupname);
-		
-		//读取icon
-		Drawable bitmap=BitmapDrawable.createFromPath(context.getCacheDir().getAbsolutePath()+"//"+ApplicationVar.getId()
-		+"//Icon"+"//"+groupname+".png");
+
+		// 读取icon
+		Drawable bitmap = BitmapDrawable.createFromPath(context.getCacheDir().getAbsolutePath() + "//"
+				+ ApplicationVar.getId() + "//Icon" + "//" + groupname + ".png");
 		groupIcon.setBackground(bitmap);
 		return convertView;
 	}
 
 	@Override
-	public View getChildView(final int groupPosition,final int childPosition, boolean isLastChild, View convertView,
+	public View getChildView(final int groupPosition, final int childPosition, boolean isLastChild, View convertView,
 			ViewGroup parent) {
 		// TODO Auto-generated method stub
-		if(convertView==null){
-			convertView=LayoutInflater.from(context).inflate(R.layout.auth_history_child, null);
+		if (convertView == null) {
+			convertView = LayoutInflater.from(context).inflate(R.layout.auth_history_child, null);
 		}
-		TextView start_time=(TextView) convertView.findViewById(R.id.start_time);
-		TextView end_time=(TextView) convertView.findViewById(R.id.end_time);
-		final CheckBox checkBox=(CheckBox)convertView.findViewById(R.id.check);
+		TextView start_time = (TextView) convertView.findViewById(R.id.start_time);
+		TextView end_time = (TextView) convertView.findViewById(R.id.end_time);
+		final CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.check);
 		String group = groups.get(groupPosition);
-		AuthNode authNode=childNodes.get(group).get(childPosition);
+		AuthNode authNode = childNodes.get(group).get(childPosition);
 		start_time.setText(authNode.getStart_time());
 		end_time.setText(authNode.getEnd_time());
-		if(isMulChoice){
+		if (isMulChoice) {
 			checkBox.setVisibility(View.VISIBLE);
-			boolean ischecked=isChecked.get(groupPosition).get(childPosition);
-			if(childPosition==0)
-				Log.e("view",ischecked+"");
+			boolean ischecked = isChecked.get(groupPosition).get(childPosition);
+			if (childPosition == 0)
+				Log.e("view", ischecked + "");
 			checkBox.setChecked(ischecked);
-		}else{
+		} else {
 			checkBox.setVisibility(View.GONE);
 		}
-		
+		if (!isAllowOption)
+			convertView.setClickable(false);
+		else
+			convertView.setClickable(true);
 		convertView.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				if(isMulChoice){
-					boolean ischecked=isChecked.get(groupPosition).get(childPosition);
+				if (isMulChoice) {
+					boolean ischecked = isChecked.get(groupPosition).get(childPosition);
 					isChecked.get(groupPosition).put(childPosition, !ischecked);
 					checkBox.setChecked(!ischecked);
 					notifyDataSetChanged();
 				}
 			}
 		});
-		
+
 		convertView.setOnLongClickListener(new OnLongClickListener() {
-			
+
 			@Override
 			public boolean onLongClick(View v) {
 				// TODO Auto-generated method stub
-				if(!isMulChoice){
-					isMulChoice=true;
-					Log.e("很好","点击了");
-					for(int i=0;i<groups.size();i++){
-						String groupname=groups.get(i);
-						int size=childNodes.get(groupname).size();
+				if (!isMulChoice) {
+					isMulChoice = true;
+					Log.e("很好", "点击了");
+					for (int i = 0; i < groups.size(); i++) {
+						String groupname = groups.get(i);
+						int size = childNodes.get(groupname).size();
 						HashMap<Integer, Boolean> maps = new HashMap<Integer, Boolean>();
 						isChecked.add(maps);
-						for(int j=0;j<size;j++){
+						for (int j = 0; j < size; j++) {
 							maps.put(j, false);
 						}
 					}
@@ -254,7 +262,7 @@ public class AuthInfoAdapter extends BaseExpandableListAdapter implements Header
 				return true;
 			}
 		});
-		
+
 		return convertView;
 	}
 
@@ -273,13 +281,13 @@ public class AuthInfoAdapter extends BaseExpandableListAdapter implements Header
 	@Override
 	public void configureHeader(View header, int groupPosition, int childPosition, int alpha) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setGroupClickStatus(int groupPosition, int status) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -287,5 +295,65 @@ public class AuthInfoAdapter extends BaseExpandableListAdapter implements Header
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	
+
+	@Override
+	public void delAuthInfoState(Info info) {
+		// TODO Auto-generated method stub
+		// 这里将会清除数据
+		if (info.isState()) {// 成功
+			AlertDialog.Builder builder = new Builder(context);
+			// 设置对话框图标，可以使用自己的图片，Android本身也提供了一些图标供我们使用
+			builder.setIcon(R.drawable.ic_launcher);
+			// 设置对话框标题
+			builder.setTitle("提示信息");
+			// 设置对话框内的文本
+			builder.setMessage("确定删除这些记录吗？");
+			// 设置确定按钮，并给按钮设置一个点击侦听，注意这个OnClickListener使用的是DialogInterface类里的一个内部接口
+			builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+					for (int i = 0; i < groups.size(); i++) {// 分组
+						String groupname = groups.get(i);
+						HashMap<Integer, Boolean> maps = isChecked.get(i);
+						Iterator<Map.Entry<Integer, Boolean>> iterator = maps.entrySet().iterator();
+						while (iterator.hasNext()) {
+							Map.Entry<Integer, Boolean> entry = iterator.next();
+							if (entry.getValue()) {// 此项被选中
+								int childpos = entry.getKey();
+								childNodes.get(groupname).remove(childNodes.get(groupname).get(childpos));
+							}
+						}
+					}
+					isAllowOption = true;// 恢复操作
+					notifyDataSetChanged();// 此时不应再进行操作
+				}
+			});
+			// 使用builder创建出对话框对象
+			AlertDialog dialog = builder.create();
+			// 显示对话框
+			dialog.show();
+		} else {
+			AlertDialog.Builder builder = new Builder(context);
+			// 设置对话框图标，可以使用自己的图片，Android本身也提供了一些图标供我们使用
+			builder.setIcon(R.drawable.ic_launcher);
+			// 设置对话框标题
+			builder.setTitle("提示信息");
+			// 设置对话框内的文本
+			builder.setMessage("删除失败！");
+			// 设置确定按钮，并给按钮设置一个点击侦听，注意这个OnClickListener使用的是DialogInterface类里的一个内部接口
+			builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+					isAllowOption = true;// 恢复操作
+				}
+			});
+			// 使用builder创建出对话框对象
+			AlertDialog dialog = builder.create();
+			// 显示对话框
+			dialog.show();
+		}
+	}
+
 }
